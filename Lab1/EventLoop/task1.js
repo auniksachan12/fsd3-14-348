6655006ba5b011f85a12c05a51b1ccc73e9fac0a
Aunik Sachan <auniksach12@gmail.com>
2026-07-23 15:35:06 +0530

    task1 stack calling

--- Lab1/EventLoop/task1.js
+++ Lab1/EventLoop/task1.js
@@ -1,17 +1,34 @@
-let f1=()=>{
-    console.log("f1");
+// JavaScript is synchronus and singlethreaded
+
+const f1 = () => {   //Synchronus calling by forming stack 
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
 };
-let f2=()=>{
-    console.log("f2");
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
 };
-let f3=()=>{
-    console.log("f3");
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
 };
-function main(){
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end main");
+function main() {
+  console.log("main starts");
+  f1();
+  console.log("end main");
 }
 main();
+
+
+// In asynchronous calling we use event loop to manage call stack   
+
+// Asynchronus call using timers-
+// 1- setTimeOut 
+// 2- setImmediate
+// 3- process.nextTick
+// 4- setInterval
