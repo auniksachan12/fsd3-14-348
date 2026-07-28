5eb234e43a2907b205372371753b8a6eead2f57b
Aunik Sachan <auniksach12@gmail.com>
2026-07-28 10:32:25 +0530

    folder curd operation

---
+++ Lab2/prg2.js
@@ -0,0 +1,9 @@
+import {mkdir,rm} from "fs/promises";
+
+// await mkdir("uploads");
+// await mkdir("uploads/images");
+// await mkdir("docs/resume/data", {recursive: true});
+// await rm('docs,resumes,data',{recursive:true});
+
+// removes main folder and sub folder also 
+await rm("docs", {recursive:true});
