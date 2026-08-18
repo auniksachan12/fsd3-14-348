3d3d0b2a830211293e8f905a5f242371611beeba
Aunik Sachan <auniksach12@gmail.com>
2026-08-18 10:31:55 +0530

    add gitignore

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,9 +1,6 @@
 import http from "http"
-const server=http.createServer();
-server.on('request',(req,res)=>{
-res.write("<h1>welcome to server side programming</h1>");
-res.write("<h2>Nodeman is treacking the files</h2>")
-res.end();
+const server=http.createServer((req,res)=>{
+    res.end("<h2>welcome to server side programming</h2>");
 });
 server.listen(5000,()=>{
     console.log("Server is running");
