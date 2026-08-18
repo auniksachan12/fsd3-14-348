c930b2699589e5b65ebd8aab38b5be79ad49c640
Aunik Sachan <auniksach12@gmail.com>
2026-08-18 10:21:06 +0530

    Backend server module start

---
+++ Lab3/app.js
@@ -0,0 +1,10 @@
+import http from "http"
+const server=http.createServer();
+server.on('request',(req,res)=>{
+res.write("<h1>welcome to server side programming</h1>");
+res.write("<h2>Nodeman is treacking the files</h2>")
+res.end();
+});
+server.listen(5000,()=>{
+    console.log("Server is running");
+});
