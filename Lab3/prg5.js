cb0a2420707238f1ee2a0d4f6350fbdefe78fb8f
Aunik Sachan <auniksach12@gmail.com>
2026-08-25 10:05:23 +0530

    prg5 done

---
+++ Lab3/prg5.js
@@ -0,0 +1,12 @@
+import http from "http"
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"content-type":"application/json"});
+    const product={
+        name:"Mobile phone",
+        price:25000,
+        discount:"10%",
+        company:"Samsung",
+    };
+    res.end(JSON.stringify(product));
+});
+server.listen(3000,()=> console.log("Server is running"));
