efdb2ea50484966ca9b648732d7a9723c29d3a39
Aunik Sachan <auniksach12@gmail.com>
2026-08-27 14:50:41 +0530

    prg8 and prg9 done

---
+++ Lab3/prg8.js
@@ -0,0 +1,23 @@
+import http from 'http';
+import {createReadStream} from "fs";
+
+const serv=http.createServer(async(req,res)=>{
+    if(req.url ==="/stream"){
+
+    
+    const stream=createReadStream("big-txt",{encoding:"utf-8"})
+    stream.pipe(res);
+    res.end();
+}
+    else if(req.url ==="/normal"){
+        const text =await readFile("big-txt");
+        res.end(text)
+    }
+    else if(req.url ==='/product'){
+        // set.writeHeader() status+content type
+        res.setHeader("content-type","text/html")
+        const data=createReadStream("product.html");
+        data.pipe(res);
+    }
+});
+serv.listen(3000,()=>console.log("running"));
--- Lab3/readme.md
+++ Lab3/readme.md
@@ -5,3 +5,18 @@ Status code
 401 - Un authorized 
 403 - Forbidden
 404 - Not found
+
+#API - Any API can be of 4 types
+GET-READ
+POST-CREATE 
+PUSH/PATCT-UPDATE
+DELETE-DELETE
+
+Api can be respond by status code and Json data.
+Api generally start with API/version
+/api/v1/products
+/api/v1/products/2169
+/api/v1/users
+/api/v1/users/AunikSachan
+
+To check other three request type like post,put,patch and delete we require frontend or third party API tester like postman , thunder client, echo API
