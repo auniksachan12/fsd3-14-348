7dd9dc8d561ba8819a50e33f0741eab8a99b576c
Aunik Sachan <auniksach12@gmail.com>
2026-09-08 09:37:33 +0530

    add teams.js

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,7 +1,7 @@
 import http from "http"
 const server=http.createServer((req,res)=>{
-    res.end("<h2>welcome to server side programming</h2>");
+    res.end("<h2>SIH internal</h2>");
 });
 server.listen(5000,()=>{
-    console.log("Server is running");
+    console.log("SIH Server is running");
 });
---
+++ Lab3/teams.js
@@ -0,0 +1,38 @@
+let teams=[{
+    id:1,tname:"Rusty",
+    tl:"Ayush sharma",
+    gmail:"ayu.sharma@gmail.com",
+    member:6,
+
+},
+{
+    id:1,tname:"hack",
+    tl:"Aryan kumar",
+    gmail:"ary.kumar@gmail.com",
+    member:6,
+
+},
+];
+let nextId=3;
+export const getAllTeams=()=>teams;
+export const getTeamById=(id)=>teams.find((team)=>team.id===id);
+
+export const addTeam=(newTeam)=>{
+    const team={id:nextId++,newTeam};
+    teams.push(team);
+    return team;    
+}
+
+export const updateTeamById=(id,updateTeam)=>{
+    const team=getTeamById(id);
+    if(!team)   return null;
+    Object.assign(team,updateTeam);
+    return team;
+}
+
+export const deleteTeam=(id)=>{
+    const index=teams.findIndex((team)=>{team.id===id});
+    if(index==-1)   return false;
+    teams.splice(index,1);
+    return true;
+};
